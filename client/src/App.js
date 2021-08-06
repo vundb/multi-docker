b9388b6cd81d4b7eff4acba84d687f8ab09b0d8e
Viktor Breitenbücher <breitenbuecher.viktor@gmail.com>
2021-08-06 22:13:40 +0200

    Change title

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ function App() {
     <Router>
       <div>
         <div className="App">
+          <h1>Fib Calculator</h1>
           <Link to="/">Home</Link> | <Link to="/otherpage">Other Page</Link>
         </div>
         <div>
