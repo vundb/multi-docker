48d1bc5018ad425117ab99409324abfe3d4f12e0
Viktor Breitenbücher <breitenbuecher.viktor@gmail.com>
2021-08-30 09:34:18 +0200

    Ping Deployment

--- deploy.sh
+++ deploy.sh
@@ -15,3 +15,4 @@ kubectl apply -f k8s
 kubectl set iamge deployments/client-deployment client=vundb/multi-docker-client:$SHA
 kubectl set image deployments/server-deployment server=vundb/multi-docker-server:$SHA
 kubectl set image deployments/worker-deployment worker=vundb/multi-docker-worker:$SHA
+
